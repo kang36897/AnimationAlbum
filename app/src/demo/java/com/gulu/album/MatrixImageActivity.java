2dcd7579651458bc116e86f87c49bba3f7ae8e03
Brian Kang <kang36897@gmail.com>
2015-09-18 22:35:13 +0800

    1.test two scale image example

---
+++ app/src/demo/java/com/gulu/album/MatrixImageActivity.java
@@ -0,0 +1,14 @@
+package com.gulu.album;
+
+import android.os.Bundle;
+
+/**
+ * Created by lulala on 18/9/15.
+ */
+public class MatrixImageActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scale_image);
+    }
+}
