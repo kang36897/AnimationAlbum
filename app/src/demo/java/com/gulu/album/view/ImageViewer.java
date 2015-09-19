32af083625261da15c5cb93b6403a811030af2b6
kang36897 <kang36897@163.com>
2015-09-19 11:13:54 +0800

    1.unfinished  image viewer

--- app/src/demo/java/com/gulu/album/view/ImageViewer.java
+++ app/src/demo/java/com/gulu/album/view/ImageViewer.java
@@ -1,11 +1,20 @@
 package com.gulu.album.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+
+import com.gulu.album.R;
 
 /**
  * Created by Administrator on 2015/9/17.
@@ -17,16 +26,33 @@ public class ImageViewer extends View {
 
     private GestureListener mGestureListener ;
 
+    private final static float SCALE_LIMIT = 2.0f;
+
+    private String mDataSource;
+
+    private Bitmap mSourceBitmap;
+    private Rect mContentBounds = new Rect();
+
+    private float mScale = 1.0f;
+    private Rect mSrc = new Rect();
+    private Rect mDst = new Rect();
+    private Rect mSrcOrignal = new Rect();
+    private Rect mDstOrignal = new Rect();
+
     public ImageViewer(Context context) {
         super(context);
+        initialization(context);
     }
 
     public ImageViewer(Context context, AttributeSet attrs) {
+
         super(context, attrs);
+        initialization(context);
     }
 
     public ImageViewer(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initialization(context);
     }
 
 
@@ -37,9 +63,72 @@ public class ImageViewer extends View {
         mScaleGestureDetector = new ScaleGestureDetector(context, mGestureListener);
         mGestureDetector = new GestureDetector(context, mGestureListener);
 
+        mSourceBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.football_girl);
 
     }
 
+    public void setPreviewImage(Bitmap bitmap){
+        mSourceBitmap = bitmap;
+    }
+
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int w = getMeasuredWidth() - getPaddingLeft() - getPaddingRight();
+        int h = getMeasuredHeight() - getPaddingTop() - getPaddingBottom();
+        mContentBounds.set(0, 0, w, h);
+
+        int dw = mSourceBitmap.getWidth();
+        int dh = mSourceBitmap.getHeight();
+        mSrc.set(0, 0, dw, dh);
+        mSrcOrignal.set(mSrc);
+
+        int nw = 0;
+        int nh = 0;
+        float scale = 1;
+        if(dw * h > w * dh)
+        {
+            nw = w;
+            scale = (float) w / (float)dw;
+            nh = (int)(dh * scale);
+
+        }else
+        {
+            nw =(int) (dw / scale);
+            nh = (int) (dh /scale);
+
+            while( nw > w || nh > h)
+            {
+                scale *= 2;
+
+                nw =(int) (nw / scale);
+                nh = (int) (nh /scale);
+            }
+
+        }
+
+        Gravity.apply(Gravity.CENTER,nw, nh, mContentBounds, mDst);
+        mDstOrignal.set(mDst);
+
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawColor(Color.BLACK);
+
+
+
+       canvas.drawBitmap(mSourceBitmap, mSrc, mDst, null);
+
+
+
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -55,7 +144,13 @@ public class ImageViewer extends View {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
 
-            return false;
+
+
+
+
+
+
+            return true;
         }
 
         @Override
